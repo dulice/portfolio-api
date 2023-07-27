db1c168eb7daf00a3f9d9232e56db6d86429d2b2
dulice <72345683+dulice@users.noreply.github.com>
2023-07-26 23:10:48 -0700

    add cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const projectRoute = require("./route/projectRoute");
 const uploadRoute = require("./route/upload");
 const app = express();
 
-app.use(cors());
+app.use(cors(["https://react-porfolio-rho.vercel.app"]));
 app.use(express.json({limit: "20mb"}));
 app.use(express.urlencoded({extended: true}))
 
